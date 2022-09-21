fafa97adbf2c3db0fc990e7441e71d491a161f54
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-09-21 22:33:13 +0700

    `CommandBlocker-0.0.1+alpha`

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# CommandBlocker
-Prevent players from using specified commands
+> This plugin will not be released on Poggit because of rule violation [S4](https://poggit.pmmp.io/rules.edit) but it will still be under active development!
+
+# General
+**Prevent players from using specified commands**
+
+# Contact
+[![Discord](https://img.shields.io/discord/986553214889517088?label=discord&color=7289DA&logo=discord)](https://discord.gg/j2X83ujT6c)\
+**You can contact me directly via Discord `NhanAZ#9115`**
---
+++ src/NhanAZ/CommandBlocker/Main.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\CommandBlocker;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\CommandEvent;
+
+class Main extends PluginBase implements Listener {
+
+	private array $blockedCommands = [];
+
+	protected function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+		$this->blockedCommands = $this->getConfig()->get("blockedCommands");
+		foreach ($this->blockedCommands as $cmd) {
+			$cmdMap = $this->getServer()->getCommandMap()->getCommand($cmd);
+			if (is_null($cmdMap)) {
+				$this->getLogger()->warning("Unknown command: $cmd");
+				$key = array_search($cmd, $this->blockedCommands);
+				unset($this->blockedCommands[$key]);
+				$this->getConfig()->set("blockedCommands", array_values($this->blockedCommands));
+				continue;
+			}
+			$aliases = $cmdMap->getAliases();
+			foreach ($aliases as $aliase) {
+				array_push($this->blockedCommands, $aliase, $cmdMap->getName(), $cmdMap->getLabel());
+			}
+		}
+	}
+
+	/**
+	 * @handleCancelled true
+	 */
+	public function onCmd(CommandEvent $event): void {
+		$cmd = explode(" ", $event->getCommand());
+		$cmd = strtolower($cmd[0]);
+		$cmd = str_replace(["\"", "'", "pocketmine:"], "", $cmd);
+		if (in_array($cmd, $this->blockedCommands)) {
+			$event->getSender()->sendMessage("§cUnknown command: §b{$cmd}§c. Use §b/help§c for a list of available commands.");
+			$event->cancel();
+		}
+	}
+}
